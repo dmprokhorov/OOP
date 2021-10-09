2aaf6fae152bcc28f06b0ba09c5c44a19ee87cb2
dmprokhorov <79521726+dmprokhorov@users.noreply.github.com>
2021-10-09 22:17:09 +0300

    lab2

---
+++ lab2/TNaryTree.h
@@ -0,0 +1,36 @@
+#ifndef TNARYTREE_H
+#define TNARYTREE_H
+#include "Rectangle.h"
+#include <exception>
+#include <string>
+
+class TNaryTree
+{
+private:
+	struct Node
+	{
+		Node(Rectangle rectangle, Node* parent, int number, int size);
+		int number, size;
+		Rectangle rectangle;
+		Node** sons;
+		Node* parent;
+		~Node();
+	};
+	Node* root;
+	int N;
+public:
+	TNaryTree();
+	TNaryTree(int);
+	TNaryTree(TNaryTree&);
+	void BuildTree(Node*&, const Node*);
+	void Update(Rectangle&&, std::string&&="");
+	void Clear(std::string&&="");
+	void DeleteSons(Node*&);
+	bool Empty();
+	double Area(std::string&&="");
+	double AreaOfSubtree(Node*);
+	friend std::ostream& operator<<(std::ostream&, TNaryTree&);
+	friend void PrintNode(std::ostream&, TNaryTree::Node*);
+	virtual ~TNaryTree();
+};
+#endif
