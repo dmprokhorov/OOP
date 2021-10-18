9048d31235797e6ace6c3f3ecab42a049cf0658c
dmprokhorov <79521726+dmprokhorov@users.noreply.github.com>
2021-10-18 11:38:10 +0300

    lab2

---
+++ lab2/main.cpp
@@ -0,0 +1,102 @@
+﻿// main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <sstream>
+#include "TNaryTree.h"
+int main()
+{
+    double S = 0.;
+    std::string string;
+    TNaryTree t1(3);
+    std::cout << t1;
+    t1.Update(Rectangle(std::cin), "cbc");
+    t1.Update(Rectangle(std::cin), "");
+    t1.Update(Rectangle(std::cin));
+    t1.Update(Rectangle(std::cin), "c");
+    t1.Update(Rectangle(std::cin), "cb");
+    t1.Update(Rectangle(std::cin), "cc");
+    std::cout << t1.getItem();
+    std::cout << t1.getItem("c");
+    std::cout << t1.getItem("cc");
+    std::cout << t1.getItem("ccc");
+    std::cout << t1.getItem("cb");
+    std::cout << t1.getItem("cbb");
+    t1.Update(Rectangle(std::cin), "cbb");
+    std::cout << t1;
+    t1.Update(Rectangle(std::cin), "cbbb");
+    if (((S = t1.Area()) == -1))
+    {
+        std::cout << "There is no such element in tree" << std::endl;
+    }
+    else
+    {
+        std::cout << "Area of subtree is " << S << std::endl;
+    }
+    if (((S = t1.Area("cbbcccbc")) == -1))
+    {
+        std::cout << "There is no such element in tree" << std::endl;
+    }
+    else
+    {
+        std::cout << "Area of subtree is " << S << std::endl;
+    }
+    t1.Update(Rectangle(std::cin), "cbc");
+    std::cout << t1;
+    t1.Update(Rectangle(std::cin), "ccb");
+    t1.Update(Rectangle(std::cin), "ccbb");
+    t1.Update(Rectangle(std::cin), "cbcb");
+    t1.Update(Rectangle(std::cin), "cbcbb");
+    if (((S = t1.Area("c")) == -1))
+    {
+        std::cout << "There is no such element in tree" << std::endl;
+    }
+    else
+    {
+        std::cout << "Area of subtree is " << S << std::endl;
+    }
+    t1.Update(Rectangle(std::cin), "cbbbc");
+    std::cout << t1;
+    t1.Update(Rectangle(std::cin), "cbbc");
+    t1.Update(Rectangle(std::cin), "cbb");
+    std::cout << t1;
+    t1.Update(Rectangle(std::cin), "cbbcb");
+    t1.Update(Rectangle(std::cin), "cbbcbb");
+    t1.Update(Rectangle(std::cin), "ccbc");
+    t1.Update(Rectangle(std::cin), "cbbcbc");
+    t1.Update(Rectangle(std::cin), "cbbd");
+    t1.Update(Rectangle(std::cin), "cbbcbbc");
+    t1.Update(Rectangle(std::cin), "cbbcbbcb");
+    std::cout << t1;
+    TNaryTree t3(t1);
+    
+    t3.Update(Rectangle(std::cin));
+    t3.Update(Rectangle(std::cin), "cbbcbbcbb");
+    t3.Update(Rectangle(std::cin), "cbbcc");
+
+    std::cout << t1 << t3;
+    
+    t1.RemoveSubTree("ccc");
+    t1.RemoveSubTree("b");
+    t1.RemoveSubTree("ccbcbb");
+    t1.RemoveSubTree("cbb");
+    std::cout << t1;
+    t1.RemoveSubTree("cbbcb");
+    std::cout << t1;
+    t1.RemoveSubTree("ccb");
+    std::cout << t1;
+    t1.RemoveSubTree();
+    std::cout << t1 << t3;
+
+    TNaryTree t2(7);
+    t2.Update(Rectangle(std::cin));
+    t2.Update(Rectangle(std::cin), "c");
+    t2.Update(Rectangle(std::cin), "cb");
+    t2.RemoveSubTree();
+
+    system("pause");
+    int a = 3;
+    int b = 4;
+    return 0;
+}
+
